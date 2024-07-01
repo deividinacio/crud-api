e5c44e22dea4ad4c6fe4154f1b479b7295937fd3
Deivid Inácio <109427261+deividinacio@users.noreply.github.com>
2024-07-01 16:20:20 -0300

    add controller e model Alunos

---
+++ app/Http/Controllers/API/AlunosController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Alunos;
+use Illuminate\Http\Request;
+
+class AlunosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alunos $alunos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Alunos $alunos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alunos $alunos)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+

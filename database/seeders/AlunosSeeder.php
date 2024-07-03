7457be06c21fa8891d38f77eb293166de74170b3
Deivid Inácio <109427261+deividinacio@users.noreply.github.com>
2024-07-03 16:29:38 -0300

    teste de inserção com Carbon e random_int

--- database/seeders/AlunosSeeder.php
+++ database/seeders/AlunosSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Alunos;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class AlunosSeeder extends Seeder
 {
@@ -15,8 +16,14 @@ class AlunosSeeder extends Seeder
      */
     public function run(): void
     {
-        Alunos::factory()
-        ->count(10)
-        ->create();
+        DB::table('aluno')-> insert([
+            'nome_aluno'=> 'Aluno Teste3',
+            'nro_registro' => random_int(1,1000), // apenas para teste 
+            'dat_inicio' => Carbon::createFromFormat('d/m/Y' ,'01/02/2023') ,
+            'dat_conclusao'=> Carbon::createFromFormat('d/m/Y' ,'01/02/2025'),
+            'dat_colacao_grau'=> Carbon::createFromFormat('d/m/Y' ,'03/03/2025'),
+            'dat_expedicao_diploma'=>Carbon::createFromFormat('d/m/Y' ,'20/04/2025')
+        ]);
+      
     }
 }
